b0d7dab1e5b3aead99589d9aeebdab0fb99d03ca
alizademhdi <alizademhdi@gmail.com>
2022-05-23 23:53:56 +0430

    add documents

--- src/ALU.sv
+++ src/ALU.sv
@@ -56,7 +56,6 @@ module ALU(
             AND_OP: data_out = data_in1 & data_in2;
             SLT_OP: data_out = $signed(data_in1) < $signed(data_in2) ? 1 : 0;
             LUI_OP: data_out = {data_in2[15:0], 16'b0};
-            //JR_OP: data_out = $signed(data_in1) < $signed(data_in2) ? 1 : 0; I dont know!!!
             BEQ_OP:
                 begin
                     if(data_in1 == data_in2)
